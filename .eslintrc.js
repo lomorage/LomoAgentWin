3ff2fb19293f09451de6b4bc754245bd6b11d123
lomolomo2 <lomorage2@gmail.com>
2023-09-05 13:31:46 -0700

    init the dev enviornment

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+const OFF = 0
+const WARN = 1
+const ERROR = 2
+
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // other configs ...
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/unicorn',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    },
+  },
+  plugins: ['react', 'unicorn', 'promise', '@typescript-eslint'],
+  rules: {
+    // 具体添加的其他规则大家可查看我的 github 查看
+    // https://github.com/vortesnail/react-ts-quick-starter/blob/master/.eslintrc.js
+  },
+}
