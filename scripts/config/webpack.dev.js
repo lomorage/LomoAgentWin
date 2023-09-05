d099498a4ac704fc08977f894c70183197d51c8c
lomolomo2 <lomorage2@gmail.com>
2023-09-05 14:08:43 -0700

    init build

--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -5,6 +5,7 @@ const { SERVER_HOST, SERVER_PORT } = require('../constant')
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
     host: SERVER_HOST, // 指定 host，不设置的话默认是 localhost
     port: SERVER_PORT, // 指定端口，默认是8080
--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = merge(common, {
   mode: 'production',
+  // devtool: 'none',
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
 })
