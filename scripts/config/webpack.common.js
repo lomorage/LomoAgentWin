eda359d02c764123f9e1bc814759faded533df8f
lomolomo2 <lomorage2@gmail.com>
2023-09-05 14:20:05 -0700

    init css package with webpack, also less and scss

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -34,5 +34,65 @@ module.exports = {
         useShortDoctype: true,
       },
     }),
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', {
+            loader: 'css-loader',
+            options: {
+              modules: false, // 默认就是 false, 若要开启，可在官网具体查看可配置项
+              sourceMap: isDev, // 开启后与 devtool 设置一致, 开发环境开启，生产环境关闭
+              importLoaders: 0, // 指定在 CSS loader 处理前使用的 laoder 数量
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: isDev,
+              importLoaders: 1, // 需要先被 less-loader 处理，所以这里设置为 1
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: isDev,
+              importLoaders: 1, // 需要先被 sass-loader 处理，所以这里设置为 1
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+    ]
+  }
 }
