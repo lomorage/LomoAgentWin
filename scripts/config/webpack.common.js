43a2267583bdddd654c26ba3465114157002239a
lomolomo2 <lomorage2@gmail.com>
2023-09-07 12:08:09 -0700

    add postcss and fix errors

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -5,6 +5,39 @@ const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const getCssLoaders = (importLoaders) => [
+  'style-loader',
+  {
+    loader: 'css-loader',
+    options: {
+      modules: false,
+      sourceMap: isDev,
+      importLoaders,
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      // ident: 'postcss',
+      postcssOptions: {
+        plugins: [
+          // 修复一些和 flex 布局相关的 bug
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            autoprefixer: {
+              grid: true,
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+          require('postcss-normalize'),
+        ],
+      },
+      sourceMap: isDev,
+    },
+  },
+]
+
 module.exports = {
   entry: {
     app: resolve(PROJECT_PATH, './src/app.js'),
@@ -15,24 +48,26 @@ module.exports = {
   },
 
   plugins: [
-  	new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: resolve(PROJECT_PATH, './public/index.html'),
       filename: 'index.html',
       cache: false, // 特别重要：防止之后使用v6版本 copy-webpack-plugin 时代码修改一刷新页面为空问题。
-      minify: isDev ? false : {
-        removeAttributeQuotes: true,
-        collapseWhitespace: true,
-        removeComments: true,
-        collapseBooleanAttributes: true,
-        collapseInlineTagWhitespace: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        minifyCSS: true,
-        minifyJS: true,
-        minifyURLs: true,
-        useShortDoctype: true,
-      },
+      minify: isDev
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
     }),
   ],
 
@@ -40,30 +75,13 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader', {
-            loader: 'css-loader',
-            options: {
-              modules: false, // 默认就是 false, 若要开启，可在官网具体查看可配置项
-              sourceMap: isDev, // 开启后与 devtool 设置一致, 开发环境开启，生产环境关闭
-              importLoaders: 0, // 指定在 CSS loader 处理前使用的 laoder 数量
-            }
-          }
-        ]
+        use: getCssLoaders(1),
       },
 
       {
         test: /\.less$/,
         use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: false,
-              sourceMap: isDev,
-              importLoaders: 1, // 需要先被 less-loader 处理，所以这里设置为 1
-            },
-          },
+          ...getCssLoaders(2),
           {
             loader: 'less-loader',
             options: {
@@ -76,15 +94,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: false,
-              sourceMap: isDev,
-              importLoaders: 1, // 需要先被 sass-loader 处理，所以这里设置为 1
-            },
-          },
+          ...getCssLoaders(2),
           {
             loader: 'sass-loader',
             options: {
@@ -93,6 +103,6 @@ module.exports = {
           },
         ],
       },
-    ]
-  }
+    ],
+  },
 }
